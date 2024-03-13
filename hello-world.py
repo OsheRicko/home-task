21784164b7b13893e4837677435bc8f6f5647312
Osher <122160788+OsheRicko@users.noreply.github.com>
2024-03-13 10:09:56 +0200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -7,4 +7,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Running Flask on 0.0.0.0 to bind to all network interfaces
+    app.run(debug=False, host='0.0.0.0', port=80)
